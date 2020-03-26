9d8a2282d87428ffbc5b203550591fb6622462b6
benweizhu <benwei.zhu@exxonmobil.com>
2020-03-26 15:31:13 +0800

    add Input basic

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from './components/Button';
 
 import './App.css';
+import Input from './components/Form/Input';
 
 function App() {
   return (
@@ -9,9 +10,13 @@ function App() {
       <div>
         <Button id="myButton" type="primary" buttonType="button" name="myButton" className="hello" onClick={() => { console.log('Hello Button') }}>Hello Button</Button>
       </div>
-      <br/>
+      <br />
       <div>
         <Button id="myLinkButton" type="primary" target="_blank" href="https://www.google.com" className="hello link" onClick={() => { console.log('Hello Link') }}>Hello Button</Button>
+      </div>
+      <br />
+      <div>
+        <Input />
       </div>
     </div>
   );
